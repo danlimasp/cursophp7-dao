6e3ecf00c2c14bc6a60107093238dd34369e8a7b
Anderson Lima <anderson.dacostalima@hotmail.com.br>
2020-10-07 09:57:25 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+require_once("config.php");
+
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+$usuariosJSON =  json_encode($usuarios);
+
+echo $usuariosJSON;
+
+ ?>
