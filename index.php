931c397a48b704238f1634e5d0741f85384febc5
Anderson Lima <anderson.dacostalima@hotmail.com.br>
2020-10-07 23:02:51 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <?php 
 
 require_once("config.php");
+
+
+//Carrega 1 usuário
 /*
-$sql = new Sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-$usuariosJSON =  json_encode($usuarios);
-
-echo $usuariosJSON;
-*/
-
 $root = new Usuario();
 $root->loadbyId(3);
 echo $root;
+*/
 
- ?>
+/*
+//Carrega uma lista de usuários
+$lista = Usuario::getList();
+$listaJSON = json_encode($lista);
+
+echo $listaJSON;
+*/
+
+/*
+//Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("der");
+$searchJSON = json_encode($search);
+echo $searchJSON;
+*/
+
+//Carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root", "987654321");
+
+echo $usuario;
+
+
+
+?>
